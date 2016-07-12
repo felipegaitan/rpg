44b9e64a5ff0ce01ee7e0df7dd62d9956ebd1349
Felipe Gaitán <felipe.gaitan.81@gmail.com>
2016-07-12 11:20:31 -0500

    Todo en un mismo archivo

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php  
+<?php
+
+    class Unit{
+
+        protected $alive = true;
+        protected $name  = true;
+
+    }
+
+    class Archer extends Unit{
+
+    }
+
+    class Soldier extends Unit{
+
+    }
+
+    class Wizard extends Unit{
+
+    }
+    
